ce493d439de3ef4bf8a3f2f6194685b65873663c
Taranko Taras <tarastarnko2@gmail.com>
2023-10-04 21:52:10 +0300

    Aded models for newton fractal and bitmap

---
+++ ComputerGraphicsProject/ComputerGraphicsProject/Models/FractalBitmapModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace ComputerGraphicsProject.Models
+{
+    public class FractalBitmapModel
+    {
+        int width;
+        int height;
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (width < 22500)
+                {
+                    width = value;
+                }
+                else
+                {
+                    throw new Exception("Width must be less than 22500");
+                }
+
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (width < 22500)
+                {
+                    height = value;
+                }
+                else
+                {
+                    throw new Exception("Height must be less than 22500");
+                }
+
+            }
+        }
+
+        Bitmap bitmap;
+        public void CreateBitMap() {
+            bitmap = new Bitmap(width, height);
+        }
+    }
+}
---
+++ ComputerGraphicsProject/ComputerGraphicsProject/Models/NewtonFractalModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ComputerGraphicsProject.Models
+{
+    public class NewtonFractalModel
+    {
+        FractalBitmapModel fractalBitmapModel;
+
+        public int MaxIterations { get; set; }
+        public double Threshold { get; set; }
+
+        int exponent = 4;
+        public NewtonFractalModel()
+        {
+            fractalBitmapModel = new FractalBitmapModel();
+            MaxIterations = 100;
+            Threshold = 0.0001;
+        }
+
+    }
+}
