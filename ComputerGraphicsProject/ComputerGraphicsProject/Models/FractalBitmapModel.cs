1300898ada7a159ae12ef20b94e9241e0e7a9765
Taranko Taras <tarastarnko2@gmail.com>
2023-10-05 15:27:12 +0300

    Fixed bitmapModel

--- ComputerGraphicsProject/ComputerGraphicsProject/Models/FractalBitmapModel.cs
+++ ComputerGraphicsProject/ComputerGraphicsProject/Models/FractalBitmapModel.cs
@@ -5,57 +5,16 @@ namespace ComputerGraphicsProject.Models
 {
     public class FractalBitmapModel
     {
-        int width;
-        int height;
-        public int Width
-        {
-            get
-            {
-                return width;
-            }
-            set
-            {
-                if (width < 22500)
-                {
-                    width = value;
-                }
-                else
-                {
-                    throw new Exception("Width must be less than 22500");
-                }
 
-            }
-        }
-        public int Height
-        {
-            get
-            {
-                return height;
-            }
-            set
-            {
-                if (width < 22500)
-                {
-                    height = value;
-                }
-                else
-                {
-                    throw new Exception("Height must be less than 22500");
-                }
-
-            }
-        }
+        public int Width { get; set; }
+        public int Height { get; set; }
 
         public Bitmap Bitmap { get; set; }
-
-        public FileResult FractalFile { get; set; }
-        public byte[] FractalBytes { get; set; }
 
         public FractalBitmapModel()
         {
             Width = 500;
             Height = 500;
-            FractalBytes = new byte[1];
         }
     }
 }
